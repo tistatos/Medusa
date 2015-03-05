a0555fd898c565b7643642b2bce7a8b0defed8b0
tistatos <tistatos@gmail.com>
2015-03-05 08:41:12 +0100

    freenect_sync tests

--- src/kinect.cpp
+++ src/kinect.cpp
@@ -4,14 +4,42 @@
  * @autor Erik Sandrén
  * @date  2015-02-27
  */
+#include <iostream>
+#include "libfreenect/libfreenect_sync.h"
 
-#include "kinect.h"
-#include "libfreenect.h"
 
-Kinect::Kinect()
-{ }
-
-int Kinect::sum(int a, int b)
+int main(int argc, char const *argv[])
 {
-  return a+b;
+  int ret;
+  char *rgb = 0;
+  uint32_t ts;
+  freenect_raw_tilt_state *state = 0;
+
+  freenect_sync_set_led(LED_RED,0);
+
+  freenect_sync_set_tilt_degs(0, 0);
+  freenect_sync_get_tilt_state(&state, 0);
+
+  while(state->tilt_angle != 0)
+  {
+    freenect_sync_get_tilt_state(&state, 0);
+    std::cout << "angle" << static_cast<int>(state->tilt_angle) << std::endl;
+  }
+
+  ret = freenect_sync_get_video((void**)&rgb, &ts, 0, FREENECT_VIDEO_RGB);
+  std::cout << "first " << ts << std::endl;
+  freenect_sync_set_tilt_degs(27, 0);
+
+  while(state->tilt_angle != 27)
+  {
+    freenect_sync_get_tilt_state(&state, 0);
+    int ang = state->tilt_angle;
+    std::cout << "angle " << ang << std::endl;
+  }
+
+  ret = freenect_sync_get_video((void**)&rgb, &ts, 0, FREENECT_VIDEO_RGB);
+  std::cout << "second " << ts << std::endl;
+  freenect_sync_set_led(LED_BLINK_GREEN,0);
+
+  return 0;
 }
