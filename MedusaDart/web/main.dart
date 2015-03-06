5e2a92368d776f6f4e4d83dcc2e0109a38cbbb83
isabelljansson <janssonisabell@gmail.com>
2015-03-06 15:29:18 +0100

    Added a start page. The start page switches to the main page when the start button is pushed. The start page is not done, but not its design

--- MedusaDart/web/main.dart
+++ MedusaDart/web/main.dart
@@ -1,8 +1,13 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+//Medusa 3D-reconstruction
+
+//Do the c++ calls here...
 
 import 'dart:html';
 
-void main() {
-  
+
+void main() 
+{
 }
+
+
+
