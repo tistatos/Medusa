7d6689f7a96872d51aad0015ae1781e4889ec9da
isabelljansson <janssonisabell@gmail.com>
2015-03-06 15:26:14 +0100

    Added a start page. The start page switches to the main page when the start button is pushed. The start page is not done, but not its design

---
+++ MedusaDart/web/js/startPage.js
@@ -0,0 +1,50 @@
+//Medusa 3D-reconstruction
+//This file handles the start up page
+//classie.js is used to easily add and remove classes
+ 
+
+(function( window ){
+  
+  var infoMenu = document.querySelector( ".infoMenu" ),
+      infoButton = document.querySelector( ".infoButton" ),
+      startButton = document.querySelector( ".startButton" ),
+      continueBotton = document.querySelector( ".continueBotton" ),
+      body = document.body,
+      mask = document.createElement("div"), 
+      activeNav;
+      
+  mask.className = "mask";
+  
+  //Slide down the start button when it's pushed
+  //Slide in instruction about the scanning process after the start button has been pushed
+  startButton.addEventListener("click", function()
+  {
+    classie.add(body, "startButton-open");
+    classie.add(body, "instructions-open");
+    document.body.appendChild(mask);
+    activeNav="startButton-open";
+  
+  });
+  
+  //Show information menu when the information button is pushed
+  infoButton.addEventListener("click", function()
+  {
+    classie.add(body, "menu-open");
+    document.body.appendChild(mask);
+    activeNav="menu-open";
+  
+  });
+  
+  //Close the information menu when the close button is pushed
+  [].slice.call(document.querySelectorAll(".closeButton")).forEach(function(el,i){
+    el.addEventListener( "click", function(){
+      classie.remove( body, activeNav );
+      activeNav = "";
+      document.body.removeChild(mask);
+    } );
+  });
+
+
+
+
+})( window );
