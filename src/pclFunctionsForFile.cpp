197af84f2321008a81cb627f3fe186a5eb8ac139
pelse862 <pelse862@student.liu.se>
2015-03-13 14:00:34 +0100

    Uppdaterad rensning och cmakelist

--- src/pclFunctionsForFile.cpp
+++ src/pclFunctionsForFile.cpp
@@ -22,15 +22,20 @@
 #include <pcl/surface/marching_cubes_hoppe.h>
 #include <pcl/surface/poisson.h>
 #include <pcl/features/integral_image_normal.h>
+#include <pcl/surface/grid_projection.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/conditional_removal.h>
+#include <pcl/common/transforms.h>
 
-
-
-
+#include <pcl/compression/octree_pointcloud_compression.h>
 
 using namespace cv;
 
 
 pcl::PointCloud<pcl::PointXYZ> loadData();
+pcl::PointCloud<pcl::PointXYZ> loadData2();
+
+void testGP(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 
 void TestPoisson(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 
@@ -42,6 +47,30 @@ void showMesh(pcl::PolygonMesh mesh);
 
 void show (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud);
 
+pcl::PointCloud<pcl::PointXYZ>::Ptr reduceData(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+{
+  pcl::ConditionAnd<pcl::PointXYZ>::Ptr range_cond ( new pcl::ConditionAnd<pcl::PointXYZ>);
+  range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::LT, 2)));
+  range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::GT, -0.1)));
+  range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("x", pcl::ComparisonOps::LT, 0.7)));
+  range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("x", pcl::ComparisonOps::GT, -0.3)));
+ 
+  pcl::ConditionalRemoval<pcl::PointXYZ> condrem (range_cond);
+  condrem.setInputCloud(cloud);
+  condrem.setKeepOrganized(true);
+  condrem.filter(*cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr decompressedCloud(new pcl::PointCloud<pcl::PointXYZ>);
+  //octree compressos object
+  pcl::io::OctreePointCloudCompression<pcl::PointXYZ>octreeCompression(pcl::io::MED_RES_ONLINE_COMPRESSION_WITHOUT_COLOR, true);
+  //stringstream that will hold the compressed cloud
+  std::stringstream compressedData;
+  //compress the cloud
+  octreeCompression.encodePointCloud(cloud,compressedData);
+  //Decompress the cloud
+  octreeCompression.decodePointCloud(compressedData, decompressedCloud);
+
+  return decompressedCloud;
+}
 
 /**
  * @brief [main]
@@ -50,23 +79,47 @@ void show (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud);
  */
 
 int main()
-
 {
   //skapar ett "rum"
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud 
-        (new pcl::PointCloud<pcl::PointXYZ>(loadData()));
-show(cloud); 
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudFront 
+      (new pcl::PointCloud<pcl::PointXYZ>(loadData()));
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudBack 
+      (new pcl::PointCloud<pcl::PointXYZ>(loadData2()));
+  std::cout << cloudFront->size() << std::endl;
+  cloudFront = reduceData(cloudFront);
+  cloudBack = reduceData(cloudBack);
 
+  Eigen::Matrix4f transform = Eigen::Matrix4f::Identity();
+  transform(2,2) = -1;
   
-//GreedyProjectionTriangulation      
-TestGp3(cloud);
+  transform(0,3) = 0.15;
+  transform(1,3) = 0.1;
+  transform(2,3) = 3;
+  std::cout << "reducerat" << std::endl;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloudback (new pcl::PointCloud<pcl::PointXYZ> ());
+  // You can either apply transform_1 or transform_2; they are the same
+  pcl::transformPointCloud (*cloudBack, *transformed_cloudback, transform);
 
-//Poisson
-TestPoisson(cloud);
 
-//Marching cubes
-TestMC(cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudAll (new pcl::PointCloud<pcl::PointXYZ>);
+  *transformed_cloudback += *cloudFront;
+  std::cout << transformed_cloudback->size() << std::endl;
+  //show(transformed_cloudback); 
+
+  //show(cloudFront); 
+
+  //show(transformed_cloudback);
+  
+  //GreedyProjectionTriangulation     
+  TestGp3(transformed_cloudback);
+
+  //Poisson
+  //TestPoisson(transformed_cloudback);
+
+  //TestMC(cloud);
+  //OFT
+  //testGP(cloud);
   
   return 0;
 }
@@ -83,7 +136,23 @@ pcl::PointCloud<pcl::PointXYZ> loadData()
  
   pcl::PointCloud<pcl::PointXYZ> cloud;
 
-  if(pcl::io::loadPCDFile<pcl::PointXYZ> ("model.pcd", cloud) == -1)
+  if(pcl::io::loadPCDFile<pcl::PointXYZ> ("carlfront.pcd", cloud) == -1)
+  {
+    std::cout << "failed to load file" << std::endl;
+    return cloud;
+  }
+
+  return cloud;
+  
+}
+
+pcl::PointCloud<pcl::PointXYZ> loadData2()
+{
+
+ 
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+
+  if(pcl::io::loadPCDFile<pcl::PointXYZ> ("carlback.pcd", cloud) == -1)
   {
     std::cout << "failed to load file" << std::endl;
     return cloud;
@@ -117,6 +186,7 @@ void show (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
     boost::this_thread::sleep (boost::posix_time::microseconds (100000));
   }
 }
+
 /**
  * @brief [View polygon mesh (surface)]
  * @details [long description]
@@ -143,7 +213,8 @@ void showMesh(pcl::PolygonMesh mesh){
  */
 void TestGp3(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 {
-
+  
+  std::cout << "normaler" << std::endl;
 //Estimate normals
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
   pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
@@ -151,10 +222,10 @@ void TestGp3(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
   tree->setInputCloud (cloud);
   n.setInputCloud (cloud);
   n.setSearchMethod (tree);
-  n.setKSearch (20);
+  n.setKSearch (3);
   n.compute (*normals);
 
-
+  std::cout << "normaler klar" << std::endl;
   pcl::PointCloud<pcl::PointNormal>::Ptr cloudWithNormals (new pcl::PointCloud<pcl::PointNormal>);
   pcl::concatenateFields(*cloud,*normals, *cloudWithNormals);
 
@@ -167,18 +238,19 @@ void TestGp3(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 
   pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
   pcl::PolygonMesh triangle;
+  std::cout << "gp3 saker" << std::endl;
+  gp3.setSearchRadius(0.05);
 
-  gp3.setSearchRadius(0.025);
-
-  gp3.setMu (2.5);
+  gp3.setMu (5);
   gp3.setMaximumNearestNeighbors (100);
-  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+  gp3.setMaximumSurfaceAngle(M_PI); // 45 degrees
   gp3.setMinimumAngle(M_PI/18); // 10 degrees
   gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
   gp3.setNormalConsistency(false);
 
   gp3.setInputCloud (cloudWithNormals);
-  gp3.setSearchMethod (tree2);
+  gp3.setSearchMethod (tree2);                                                     
+  std::cout << "rekonstruera" << std::endl;
   gp3.reconstruct (triangle);
 
   // Additional vertex information
@@ -186,11 +258,11 @@ void TestGp3(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
   std::vector<int> states = gp3.getPointStates();
   //saves to an obj file
   //pcl::io::saveOBJFile ("mesh.obj", triangle, 5);
+  std::cout << "show mesh" << std::endl;
 
-  float ISOLevel = 0.5;
-  float leafSize = 0.01f;
   showMesh(triangle);
 }
+
 /**
  * @brief [Surface reconstruction using poisson]
  * @details [long description]
@@ -200,15 +272,17 @@ void TestGp3(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 void TestPoisson(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 {
   
-  
+  std::cout << cloud->size() << std::endl; 
+
+
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
   tree->setInputCloud (cloud);
  
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
   n.setInputCloud (cloud);
   n.setSearchMethod (tree);
-  n.setKSearch (5);
-
+  n.setKSearch (20);
+  //n.setRadiusSearch(5);
   pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
   n.compute (*normals);
  
@@ -246,7 +320,7 @@ void TestMC(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
   n.setInputCloud (cloud);
   n.setSearchMethod (tree);
   n.setKSearch (5);
-
+ // n.setRadiusSearch(0.03);
   pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
   n.compute (*normals);
  
@@ -269,5 +343,38 @@ void TestMC(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
   
   showMesh(mesh);
  
+
+}
+
+void testGP(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud (cloud);
+ 
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+  n.setInputCloud (cloud);
+  n.setSearchMethod (tree);
+  n.setKSearch (15);
+ // n.setRadiusSearch(0.03);
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+  n.compute (*normals);
+ 
+
+ // std::cout << "normals " << normals->size() << std::endl;
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloudAndNormals (new pcl::PointCloud<pcl::PointNormal>);
+  pcl::concatenateFields(*cloud,*normals, *cloudAndNormals);
+  pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+  tree2->setInputCloud(cloudAndNormals);
+
+
+  pcl::PolygonMesh mesh;
+
+  pcl::GridProjection<pcl::PointNormal> gp;
+
+  gp.setInputCloud(cloudAndNormals);
+  gp.setSearchMethod(tree2);
+  gp.setResolution(0.001);
+  gp.reconstruct(mesh);
+
+  showMesh(mesh);
 
 }
