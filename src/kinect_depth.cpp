a1d23e7868ef2d9c69cf214f8a2856c11a1f65d4
pelse862 <pelse862@student.liu.se>
2015-03-18 15:00:47 +0100

    Class files for kinect and rendering

--- src/kinect_depth.cpp
+++ src/kinect_depth.cpp
@@ -6,40 +6,48 @@
  */
 
 #include <vector>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 #include <iostream>
-#include "libfreenect.hpp"
+#include "libfreenect/libfreenect.hpp"
+#include <string.h>
+#include "kinect_depth.h"
 
-Freenect::Freenect freenect;
 
-class MyFreenectDevice : public Freenect::FreenectDevice
-{
-public:
-  MyFreenectDevice(freenect_context *ctx, int index) : Freenect::FreenectDevice(ctx,index),
-    m_buffer_video(freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB).bytes),
-    m_buffer_depth(freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_REGISTERED).bytes / 2),
-    m_new_rgb_frame(false), m_new_depth_frame(false)
+
+  void MyFreenectDevice::VideoCallback(void *video, uint32_t timestamp)
   {
-    setDepthFormat(FREENECT_DEPTH_REGISTERED);
+    if(m_new_rgb_frame)
+      return;
+    mrgb = static_cast<uint8_t*>(video);
+    copy(mrgb, mrgb+getVideoBufferSize(), m_buffer_video.begin());
+
+    m_new_rgb_frame = true;
   }
 
-  void VideoCallback(void *video, uint32_t timestamp)
+  void MyFreenectDevice::DepthCallback(void *_depth, uint32_t timestamp)
   {
-    std::cout << "Video!" << std::endl;
+    if(m_new_depth_frame)
+      return;
+    uint16_t* depth = static_cast<uint16_t*>(_depth);
+    copy(depth, depth+getDepthBufferSize()/2, m_buffer_depth.begin());
+
+    float f = 595000.f;
+    for (int i = 0; i < 640*480; ++i)
+    {
+      cloud.points[i].x = (i%640 - (640-1)/2.f) * depth[i] / f;  // X = (x - cx) * d / fx
+      cloud.points[i].y = (i/640 - (480-1)/2.f) * depth[i] / f;  // Y = (y - cy) * d / fy
+      cloud.points[i].z = depth[i]/1000.f; // Z = d
+    }
+    m_new_depth_frame = true;
   }
 
-  void DepthCallback(void *video, uint32_t timestamp)
+  void MyFreenectDevice::savePointCloud(std::string filename)
   {
+    std::cout << cloud.points.size() << std::endl;
+    pcl::io::savePCDFileASCII (filename, cloud);
+  
   }
-private:
-  std::vector<uint8_t> m_buffer_video;
-  std::vector<uint16_t> m_buffer_depth;
-  bool m_new_rgb_frame;
-  bool m_new_depth_frame;
-};
 
-int main(int argc, char const *argv[])
-{
-  MyFreenectDevice* device;
-  device = &freenect.createDevice<MyFreenectDevice>(0);
-  return 0;
-}
+
+  
