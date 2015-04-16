31048aea8428ca9fa2415e44af887d7e86ccf9c2
Carl Englund <englund92@gmail.com>
2015-04-16 15:57:12 +0200

    Erik fixed the cmakelist, can now continue with integerating the MongoDB

--- src/mongo.cpp
+++ src/mongo.cpp
@@ -2,9 +2,9 @@
 
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/json.hpp>
-
-#include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+#include <mongocxx/client.hpp>
+
 
 int main(int, char**) {
     mongocxx::instance inst{};
