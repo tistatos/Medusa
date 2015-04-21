f8db32545d2432b7cb8e50c289462bacc5b1040d
Carl Englund <englund92@gmail.com>
2015-04-21 10:36:07 +0200

    Insertion into MongoDB is now working. Created a function which takes a string parameter fileName. Just send the filename of what you want to store and it should store the correct file. After that we can just delete our file from the temp folder. Cheers! :beers:

--- include/renderMesh.h
+++ include/renderMesh.h
@@ -60,6 +60,9 @@ class renderMesh
 
 		static pcl::PointCloud<pcl::PointXYZ>::Ptr smoothing (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 
+		static void storeFile(string fileName);
+
+
 };
 
 #endif
