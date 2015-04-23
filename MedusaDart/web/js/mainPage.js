ec5b4ea8984475661a6b5d22278b5286afaa1777
cecla <cicci.lagerwall@gmail.com>
2015-04-23 11:13:41 +0200

    added some changes in mainpage

--- MedusaDart/web/js/mainPage.js
+++ MedusaDart/web/js/mainPage.js
@@ -2,25 +2,43 @@
 {
 
   var save = document.querySelector( ".save" ),
-      popupQR = document.querySelector( ".qr" ),
+      //popupQR = document.querySelector( ".qr" ),
       popupMail = document.querySelector( ".mail" ),
       body = document.body,
       mask = document.createElement("div"), 
-      activeNav;
+      activeNav,
+      quit = document.querySelector("#quit");
   var clicked = false;
       
   mask.className = "mask";
+
+  quit.addEventListener("click", function()
+  {
+    swal({
+      title: "Är du säker?",
+      text: "Din modell kommer nu att kastas!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Ja, kasta den!",
+      cancelButtonText: "Avbryt",
+      closeOnConfirm: false
+    },
+    function(){
+      swal("Kastad!", "Din modell är nu borta.. för alltid!", "success");
+    });
+  });
   
   save.addEventListener("click", function()
   {
   
-    if(!clicked)
+    /*if(!clicked)
     {
       clicked = true;
-      classie.add(body, "qr-open");
+      //classie.add(body, "qr-open");
       classie.add(body, "mail-open");
       document.body.appendChild(mask);
-      classie.remove(body, "qr-close");
+      //classie.remove(body, "qr-close");
       classie.remove(body, "mail-close");
       activeNav="save-open";  
     }
@@ -28,14 +46,14 @@
     else
     {
       clicked = false;
-      classie.add(body, "qr-close");
+      //classie.add(body, "qr-close");
       classie.add(body, "mail-close");
-      classie.remove(body, "qr-open");
+      //classie.remove(body, "qr-open");
       classie.remove(body, "mail-open");
       
       document.body.appendChild(mask);
       activeNav="save-open";    
-    }
+    }*/
   });
   /*
   popupQR.addEventListener("click", function()
