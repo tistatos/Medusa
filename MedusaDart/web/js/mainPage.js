6cc35d3c6101cc2e681cc170d263ec10a934a0c7
CarolineGard <caroline.gard@hotmail.com>
2015-04-23 11:09:42 +0200

    Added popups for mainPage

--- MedusaDart/web/js/mainPage.js
+++ MedusaDart/web/js/mainPage.js
@@ -4,16 +4,53 @@
   var save = document.querySelector( ".save" ),
       popupQR = document.querySelector( ".qr" ),
       popupMail = document.querySelector( ".mail" ),
+      redo = document.querySelector(".redo"),
+      quit = document.querySelector(".quit"),
       body = document.body,
       mask = document.createElement("div"), 
       activeNav;
+  
   var clicked = false;
       
   mask.className = "mask";
   
-  save.addEventListener("click", function()
+  quit.addEventListener("click", function()
   {
-  
+    if(!clicked)
+    {
+      swal({
+        title: "Är du säker på att du vill avsluta?",
+        text: "Den nuvarande modellen kommer att kastas!",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Avsluta",
+        closeOnConfirm: false
+      },
+      function(){
+        //swal("Deleted!", "Your imaginary file has been deleted.", "success");
+      });
+     }
+   });
+
+  redo.addEventListener("click", function()
+  {
+    if(!clicked)
+    {
+      swal({
+        title: "Är du säker på att du vill göra om modellen?",
+        text: "Den nuvarande modellen kommer att kastas!",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Gör om!",
+        closeOnConfirm: false
+      },
+      function(){
+        window.location.replace("countDown.html"); 
+      });
+    }
+  });
+
+    /*
     if(!clicked)
     {
       clicked = true;
@@ -35,17 +72,14 @@
       
       document.body.appendChild(mask);
       activeNav="save-open";    
-    }
-  });
+    }*/
+
   /*
   popupQR.addEventListener("click", function()
   {
     classie.add(body, ".popupMail.is-visible ");
     document.body.appendChild(mask);
   });*/
-  
-  
-  
  
 })( window );
 
