172c045b19446886569a32e5914c52fec9fc5189
Carl Englund <englund92@gmail.com>
2015-04-27 10:44:24 +0200

    Setting up GH-pages on Medusrepo instead

---
+++ js/mainPage.js
@@ -0,0 +1,80 @@
+(function( window )
+{
+
+  var save = document.querySelector( ".save" ),
+      //popupQR = document.querySelector( ".qr" ),
+      popupMail = document.querySelector( ".mail" ),
+      redo = document.querySelector("#reDo"),
+      body = document.body,
+      mask = document.createElement("div"), 
+      activeNav,
+      quit = document.querySelector("#quit");
+      confirm = document.querySelector(".confirm");
+  var clicked = false;
+      
+  mask.className = "mask";
+
+  save.addEventListener("click", function()
+  {
+   // sweetAlert('Congratulations!', 'Your message has been successfully sent', 'success');
+    swal({  title: "Maila mig min 3D-modell!",   
+            text: "Skriv in din mailadress:",   
+            type: "input",   showCancelButton: true,   
+            closeOnConfirm: false,
+            confirmButtonColor: "#7EC0EA",
+            confirmButtonText: "Skicka",
+            cancelButtonText: "Avbryt",  
+            inputPlaceholder: "Din mail" }, 
+            function(inputValue){   if (inputValue === false) return false;      
+                                    if (inputValue === "") {     swal.showInputError("Du har inte fyllt i någon mailadress!");     
+                                    return false   }      swal("Modellen är skickad till", inputValue); });
+    });
+
+  quit.addEventListener("click", function()
+  {
+    swal(
+    {
+      title: "Är du säker?",
+      text: "Din modell kommer nu att kastas om du inte har sparat den!",
+      showCancelButton: true,
+      confirmButtonColor: "#7EC0EA",
+      confirmButtonText: "Ja, kasta den!",
+      cancelButtonText: "Avbryt",
+      closeOnConfirm: false
+    },
+    function()
+    {
+      swal(
+      {
+        title: "Din modell har nu kastats!",
+        type: "success",
+        confirmButtonColor: "#7EC0EA",
+        confirmButtonText: "OK!"
+      },
+        function()
+        {
+          window.location.replace("index.html");
+        });
+    });
+  });
+
+  redo.addEventListener("click", function()
+  {
+      swal({
+        title: "Är du säker på att du vill göra om modellen?",
+        text: "Den nuvarande modellen kommer att kastas!",
+        showCancelButton: true,
+        confirmButtonColor: "#7EC0EA",
+        confirmButtonText: "Gör om!",
+        cancelButtonText: "Avbryt",
+        closeOnConfirm: false
+      },
+      function(){
+        window.location.replace("countDown.html"); 
+      });
+  });
+
+ 
+})( window );
+
+//alert("Hello World!");
---
+++ js/startPage.js
@@ -0,0 +1,59 @@
+//Medusa 3D-reconstruction
+//This file handles the start up page
+//classie.js is used to easily add and remove classes
+
+(function( window )
+{
+  
+  var infoMenu = document.querySelector( ".infoMenu" ),
+      infoButton = document.querySelector( ".infoButton" ),
+      startButton = document.querySelector( ".startButton" ),
+      continueButton = document.querySelector( "#continueButton" ),
+      instructionsMenu = document.querySelector( "instructions"),
+      body = document.body,
+      mask = document.createElement("div"), 
+      activeNav,
+      activeNav2;
+      
+  mask.className = "mask";
+  
+  //Show information menu when the information button is pushed
+  infoButton.addEventListener("click", function()
+  {
+    classie.add(body, "menu-open");
+    document.body.appendChild(mask);
+    activeNav2="menu-open";
+  
+  });
+  
+  //Close the information menu when the close button is pushed
+  [].slice.call(document.querySelectorAll(".closeButton")).forEach(function(el,i){
+    el.addEventListener( "click", function(){
+      classie.remove( body, activeNav2 );
+      activeNav2 = "";
+      document.body.removeChild(mask);
+    } );
+  });
+
+  //Slide down the start button when it's pushed
+  //Slide in instruction about the scanning process after the start button has been pushed
+  startButton.addEventListener("click", function()
+  {
+    classie.add(body, "startButton-open");
+    classie.add(body, "instructions-open");
+    document.body.appendChild(mask);
+    activeNav="startButton-open";
+  });
+
+  //Slide down the instructions menu when continueButton is pushed
+  //Slide in Progressbar when continueButton is pushed
+  continueButton.addEventListener("click", function()
+  {
+    //window.location = "../mainPage.html";
+    classie.add(body, "instructions-open2");
+    classie.add(body, "inProgress-open");
+    document.body.appendChild(mask);
+    activeNav="instructions"; 
+  });
+
+})( window );
